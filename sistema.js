0c62b78269820556861c9c5a9c999f4346d7dcb8
willkimkr <willkimkr@outlook.com>
2022-04-19 10:03:16 -0300

    SistemaAluno

---
+++ sistema.js
@@ -0,0 +1,75 @@
+let nomeDeAluno = ["Marcel", "Franklin", "Rita", "Afranio" ]
+
+for (let nome of nomeDeAluno) {
+    console.log(`Esta pessoa se chama ${nome}`)  
+} 
+
+
+
+
+
+// let numeroDeAlunos = 10;
+
+// let contador = 0;
+
+// while(contador <= numeroDeAlunos) {
+//    //console.log(contador)
+
+//     if(contador == 0){
+//         console.log("O numero atual e zero")
+//     }else if(contador % 2 != 0){
+//         console.log("O numero " + contador + " e Impar")
+//     }else{
+//         console.log(`O numero ${contador} e Par`)
+//     }
+//     contador++;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let  numeroDeAlunos = 10;
+
+// for (let contador = 0; contador <= numeroDeAlunos; contador++) {
+//    //console.log(contador)
+
+//     if(contador == 0){
+//         console.log("O numero atual e zero")
+//     }else if(contador % 2 == 0){
+//         console.log("O numero " + contador + " e Par")
+//     }else{
+//         console.log(`O numero ${contador} e Impar`)
+//     }
+// }
+
+
+
+
+
+
+
+
+// let numeroDeAlunos = 10;
+
+// for (let index = 0; index < Array.length; index++) {
+//     const element = array[index];
+
+// }
